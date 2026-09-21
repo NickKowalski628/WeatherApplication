f8da4a908fad745357496cf743953ca1f7955c4f
NickKowalski628 <snowred500@gmail.com>
2026-09-21 00:55:12 -0400

    Add project files.

---
+++ WeatherApplication/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+namespace WeatherApp;
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        WeatherService service = new WeatherService();
+        bool running = true;
+
+        while (running)
+        {
+            Console.WriteLine("Hello and welcome to my weather application!");
+            Console.WriteLine("1.Get forecast");
+            Console.WriteLine("2.Change zip code");
+            Console.WriteLine("3.Exit");
+
+            String choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "1":
+                    await service.TestApiCall();
+                    break;
+
+                case "2":
+                    Console.WriteLine($"Your current zipcode is:{service.GetZip()}");
+                    bool success = false;
+                    while (!success)
+                    {
+                        Console.WriteLine("What would you like to change the zip code to? ");
+                        String newZip = Console.ReadLine();
+                        success = service.setZip(newZip);
+
+
+
+                    }
+                    break;
+
+
+
+                case "3":
+                    //exit
+                    Console.WriteLine("Thanks for using me, goodbye!");
+                    running = false;
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice, please try again");
+                    break;
+            }
+
+        }
+    }
+}
---
+++ WeatherApplication/WeatherService.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO.Compression;
+
+namespace WeatherApp;
+
+public class WeatherService
+{
+    String zip = "32817";
+
+
+    public async Task TestApiCall()
+    {
+        String apiKey = "REDACTED";
+        String url = $"https://api.openweathermap.org/data/2.5/forecast?zip={zip},us&appid={apiKey}&units=imperial";
+
+        try
+        {
+            using HttpClient client = new HttpClient();
+            string response = await client.GetStringAsync(url);
+
+            Console.WriteLine(response);
+
+        }
+        catch (HttpRequestException ex)
+        {
+            Console.WriteLine("Sorry, weather services are not available at this time. Please try again later.\n");
+        }
+
+    }
+    public String GetZip()
+    {
+        return zip;
+    }
+
+
+
+
+    public bool setZip(string newZip)
+    {
+        if (newZip.Length != 5)
+        {
+            Console.WriteLine("Invalid entry. Please enter a 5 digit zipcode.");
+            return false;
+        }
+        for (int i = 0; i < newZip.Length; i++)
+        {
+            if (!char.IsDigit(newZip[i]))
+            {
+                Console.WriteLine("Zip codes only contain numbers. ");
+                return false;
+            }
+        }
+        zip = newZip;
+        return true;
+
+    }
+}
